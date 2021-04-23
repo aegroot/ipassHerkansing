2d62fafda95ff2a637468073d03fff7a5fd3aab6
aegroot <alex.a.groot@student.hu.nl>
2021-04-23 09:17:27 +0200

    init

--- src/main/java/com/example/demo/domain/message/SpringMessageRepository.java
+++ src/main/java/com/example/demo/domain/message/SpringMessageRepository.java
@@ -3,4 +3,5 @@ package com.example.demo.domain.message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpringMessageRepository extends JpaRepository<Message,Long> {
+
 }
